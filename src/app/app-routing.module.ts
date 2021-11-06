3bf2429fdcae3c329f21d015555f34aa1b4ff62d
maximilien0405 <maximilien.zimmermann@ik.me>
2021-11-07 00:17:37 +0100

    remove BIG adding useless

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AllWorkComponent } from './all-work/all-work.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { WorkDetailsComponent } from './work-details/work-details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'projet/:name', component: WorkDetailsComponent},
-  { path: 'projets', component: AllWorkComponent },
   { path: '**', redirectTo: '/' }
 ];
 
