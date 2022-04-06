d296117882ce5edc0458cda078c7a478216ff4ca
maximilien0405 <maximilien.zimmermann@ik.me>
2022-04-06 22:29:44 +0200

    added text to project subtitle

--- src/app/all-work/all-work.component.ts
+++ src/app/all-work/all-work.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { ProjectsService } from '../projects.service';
 import { Work } from './../common/work.model'
 
@@ -17,6 +17,15 @@ export class AllWorkComponent implements OnInit {
   public lang = this.translate.currentLang
 
   constructor(private projectService: ProjectsService, private router: Router, private translate: TranslateService) {
+    this.translate.onLangChange
+    .subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang
+      this.reloadData()
+    });
+  }
+
+  reloadData() {
+    this.component_all_work = [...this.component_all_work];
   }
 
   ngOnInit(): void {
