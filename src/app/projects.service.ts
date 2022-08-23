3f402989d6937c9e4355bde8d42b9f482ba48eea
maximilien0405 <maximilien.zimmermann@ik.me>
2022-08-23 14:02:30 +0200

    style(app): small edits

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -13,6 +13,6 @@ export class ProjectsService {
   private readonly API_URL = environment.api;
 
   public getAllWork(lang:any): Observable<any> {
-    return this.http.get<any>(`${this.API_URL}/api/projects?locale=${lang}&populate[0]=content.images&populate[1]=images&populate[2]=links&populate[3]=tags&populate[4]=preview_image`).pipe(map(res => res));
+    return this.http.get<any>(`${this.API_URL}/api/projects?locale=${lang}&populate[0]=content.image&populate[1]=images&populate[2]=links&populate[3]=tags&populate[4]=preview_image`).pipe(map(res => res));
   }
 }
