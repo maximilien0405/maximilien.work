2da7cfed46f40eef5b66ac628b20a405b6a3f570
maximilien0405 <maximilien.zimmermann@ik.me>
2022-09-29 10:17:58 +0200

    feat(service): start of new service page

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html'
 })
 export class ServicesComponent implements OnInit {
+  public lang = this.translate.currentLang;
 
-  constructor() { }
+
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange
+    .subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang
+    });
+  }
 
   ngOnInit(): void {
   }
