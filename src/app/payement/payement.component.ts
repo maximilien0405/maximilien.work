8d29de5b9e41d5d54a2aadff73742be4b016afdb
maximilien0405 <maximilien.zimmermann@ik.me>
2023-08-03 13:27:40 +0200

    feat(app): start of payement page

---
+++ src/app/payement/payement.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payement',
+  templateUrl: './payement.component.html'
+})
+export class PayementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
