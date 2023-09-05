0d56c6029274cb1c6aac8ed700d46a3b3439ea5c
maximilien0405 <maximilien.zimmermann@ik.me>
2023-09-05 14:21:43 +0200

    feat(all-work): add category system & other edits

--- src/app/all-work/all-work.component.ts
+++ src/app/all-work/all-work.component.ts
@@ -10,11 +10,13 @@ import { ProjectsService } from '../common/services/projects.service';
 })
 export class AllWorkComponent {
   public all_work:any;
-  public total_all_work:any = [];
-  public total_all_other:any = [];
+  public total_all_personnal:any = [];
+  public total_all_other: any = [];
+  public total_all_freelance: any = [];
+
+  public total_selected_work: any = [];
   public lang = localStorage.getItem('lang')
   public readonly API_URL = environment.api;
-  public type:number = 1;
 
   constructor(private projectService:ProjectsService,
     private router: Router,
@@ -29,12 +31,16 @@ export class AllWorkComponent {
       this.all_work = res.data;
 
       for (let x in this.all_work) {
-        if(this.all_work[x].attributes.type == 'project') {
-          this.total_all_work.push(this.all_work[x].attributes)
-        } else if(this.all_work[x].attributes.type == 'other'){
+        if (this.all_work[x].attributes.category == 'personnal') {
+          this.total_all_personnal.push(this.all_work[x].attributes)
+        } else if (this.all_work[x].attributes.category == 'other'){
           this.total_all_other.push(this.all_work[x].attributes)
+        } else if (this.all_work[x].attributes.category == 'freelance') {
+          this.total_all_freelance.push(this.all_work[x].attributes)
         }
       }
+
+      this.total_selected_work = this.total_all_personnal;
     });
   }
 
@@ -44,7 +50,13 @@ export class AllWorkComponent {
   }
 
   // Change the type
-  public changeType(type:number):void {
-    this.type = type;
+  public changeType(type: string): void {
+    if (type == 'personnal') {
+      this.total_selected_work = this.total_all_personnal;
+    } else if (type == 'other') {
+      this.total_selected_work = this.total_all_other;
+    } else if (type = 'freelance') {
+      this.total_selected_work = this.total_all_freelance;
+    }
   }
 }
